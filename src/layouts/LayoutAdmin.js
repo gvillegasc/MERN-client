6b205d765099c6bf5bd72dc09e66e1b808e7c30e
gvillegasc <villegasgerardo.j@gmail.com>
2020-04-04 18:15:18 -0500

    Creación de sistema de rutas en el admin

--- src/layouts/LayoutAdmin.js
+++ src/layouts/LayoutAdmin.js
@@ -1,12 +1,34 @@
 import React from 'react';
-import { Layout } from 'antd';
+import { Route } from 'react-router-dom';
+import { Layout, LocaleProvider } from 'antd';
 
-export default function LayoutAdmin() {
+import './LayoutAdmin.scss';
+
+export default function LayoutAdmin(props) {
+	const { routes } = props;
+	const { Header, Content, Footer } = Layout;
+	console.log(props);
 	return (
 		<Layout>
 			<h2>Menu Sidebar Admin</h2>
-			<div>Contenido</div>
-			<h5>Footer</h5>
+			<Layout>
+				<Header>Header</Header>
+				<Content>
+					<LoadRouters routes={routes} />
+				</Content>
+				<Footer>Gerardo Villegas 2020</Footer>
+			</Layout>
 		</Layout>
 	);
+}
+
+function LoadRouters({ routes }) {
+	return routes.map((route, index) => (
+		<Route
+			key={index}
+			path={route.path}
+			exact={route.exact}
+			component={route.component}
+		/>
+	));
 }
